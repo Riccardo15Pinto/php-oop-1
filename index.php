e6ace01845af216850105072eb36b73ecf74234a
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-21 16:01:07 +0200

    add space

--- index.php
+++ index.php
@@ -52,9 +52,13 @@ class Person
         return $this->getFullName() . " " . $this->age . " anni";
     }
 }
+
 $first_film = new Movie("Avengers:Infinity War", "3:00:00", "USA", "Lorem ipsum Thanos fecit dolor a muy part of the universo.....", "Action", new Person("Thanos", "Campanaros", "300"));
+
 $second_film = new Movie("Io sono Leggenda", "2:30:00", "USA", "Lorem ipsum Will Smith and the dog fecit danni.....", "Action", new Person("will", "Smith", "57"));
+
 $movies = [$first_film, $second_film];
+
 ?>
 
 <!DOCTYPE html>
